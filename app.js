f4261e885e9cb2f511d0d1a9e3d7cb4e8f37550e
smdmori <mortezasamadi1371@gmail.com>
2023-03-05 21:30:46 +0330

    changed piano branch for github pages

--- app.js
+++ app.js
@@ -1,11 +1,80 @@
-let number = 1
+const audioContext = new AudioContext()
 
-function printNumber(number) {
-  if (number > 10) return
+const NOTE_DETAILS = [
 
-  console.log(number)
-  number++
-  printNumber(number)
+  // Go to comment in playNotes() for more information
+  // { note: 'C', key: 'Z', frequency: 261.626 },
+  
+  // Cleaner Code
+  { note: 'C', key: 'Z', frequency: 261.626, active: false },
+  { note: 'Db', key: 'S', frequency: 277.183, active: false },
+  { note: 'D', key: 'X', frequency: 293.665 , active: false},
+  { note: 'Eb', key: 'D', frequency: 311.127 , active: false},
+  { note: 'E', key: 'C', frequency: 329.628 , active: false},
+  { note: 'F', key: 'V', frequency: 349.228 , active: false},
+  { note: 'Gb', key: 'G', frequency: 369.994 , active: false},
+  { note: 'G', key: 'B', frequency: 391.995 , active: false},
+  { note: 'Ab', key: 'H', frequency: 415.305 , active: false},
+  { note: 'A', key: 'N', frequency: 440 , active: false},
+  { note: 'Bb', key: 'J', frequency: 466.164 , active: false},
+  { note: 'B', key: 'M', frequency: 493.883 , active: false},
+]
+
+document.addEventListener('keydown', e=> {
+  if (e.repeat) return
+
+  const keyCode = e.code
+  const noteDetail = getNoteDetails(keyCode)
+
+  if (noteDetail == null) return
+
+  noteDetail.active = true
+  playNotes()
+})
+
+document.addEventListener('keyup', e => {
+  const keyCode = e.code
+  const noteDetail = getNoteDetails(keyCode)
+
+  if (noteDetail == null) return
+  
+  noteDetail.active = false
+  playNotes()
+})
+
+function getNoteDetails(keyPressed) {
+  return NOTE_DETAILS.find(n => `Key${n.key}` === keyPressed)
 }
 
-printNumber(number)
+function playNotes() {
+  NOTE_DETAILS.forEach(n => {
+    const keyElement = document.querySelector(`[data-note="${n.note}"]`)
+
+    // 1. this is a trick to assing 'false' to an 'undefined' value
+    // 2. this brings the readablitiy lower, so we define 'false' value in the object itself
+    // keyElement.classList.toggle('active', n.active || false)
+    keyElement.classList.toggle('active', n.active)
+
+    if (n.oscillator != null) {
+      n.oscillator.stop()
+      n.oscillator.disconnect()
+    }
+  })
+
+  const activeNotes = NOTE_DETAILS.filter(n => n.active)
+  const gain = 1 / activeNotes.length
+  activeNotes.forEach(n => {
+    startNote(n, gain)
+  })
+}
+
+function startNote(noteDetail, gain) {
+  const gainNode = audioContext.createGain()
+  gainNode.gain.value = gain
+  const oscillator = audioContext.createOscillator()
+  oscillator.frequency.value = noteDetail.frequency
+  oscillator.type = 'sine'
+  oscillator.connect(gainNode).connect(audioContext.destination)
+  oscillator.start()
+  noteDetail.oscillator = oscillator
+}
--- traversing-in-the-dom/script.js
+++
@@ -1,7 +0,0 @@
-const childOne = document.querySelector('#child-one')
-
-childOne.style.color = 'red'
-
-childOne.parentElement.style.color = 'blue'
-
-childOne.closest('#grand-parent').style.color = 'yellow'
