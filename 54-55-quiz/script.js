aac4cee85d9bfd327f941a5c70249f039846cd06
smdmori <mortezasamadi1371@gmail.com>
2023-03-06 22:53:25 +0330

    started exercise

--- 54-55-quiz/script.js
+++ 54-55-quiz/script.js
@@ -5,12 +5,34 @@
     * BONUS: The questions (have the class `question-item`)
     * BONUS: The alert (has the id `alert`)
 */
+const quizForm = document.querySelector('#quiz-form')
+const answers = document.querySelectorAll('.answer')
+const questionItems = document.querySelectorAll('.question-item')
+const alert = document.querySelector('#alert')
 
 // TODO: 3. Create a submit event listener for the form that does the following.
-//    1. Prevent the default behaviour
-//    2. Get all selected answers (use the `checked` property on the input to determine if it is selected or not)
-//    3. Loop through the selected answer to see if they are correct or not (Check the value of the answer to see if it is the string "true")
-//    4. For each correct answer add the class `correct` to the parent with the class `question-item` and remove the class `incorrect`.
-//    5. For each incorrect answer add the class `incorrect` to the parent with the class `question-item` and remove the class `correct`.
-//    6. BONUS: Make sure unanswered questions show up as incorrect. The easiest way to do this is to add the incorrect class and removing the correct class from all question items before checking the correct answers
-//    7. BONUS: If all answers are correct show the element with the id `alert` and hide it after one second (look into setTimeout) (use the class active to show the alert and remove the class to hide it)
+quizForm.addEventListener('submit', e => {
+  //    1. Prevent the default behaviour
+  e.preventDefault()
+  //    2. Get all selected answers (use the `checked` property on the input to determine if it is selected or not)
+  const answersArray = Array.from(answers)
+  const selectedAnswers = answersArray.filter(answer => {
+    if (answer.checked === true) {
+      return answer
+    }
+  })
+  console.log(selectedAnswers)
+  //    3. Loop through the selected answer to see if they are correct or not (Check the value of the answer to see if it is the string "true")
+  const correctAnswers = selectedAnswers.filter(item => {
+    if (item.value === 'true') {
+      return item
+    }
+  })
+  console.log(correctAnswers)
+
+  //    4. For each correct answer add the class `correct` to the parent with the class `question-item` and remove the class `incorrect`.
+
+  //    5. For each incorrect answer add the class `incorrect` to the parent with the class `question-item` and remove the class `correct`.
+  //    6. BONUS: Make sure unanswered questions show up as incorrect. The easiest way to do this is to add the incorrect class and removing the correct class from all question items before checking the correct answers
+  //    7. BONUS: If all answers are correct show the element with the id `alert` and hide it after one second (look into setTimeout) (use the class active to show the alert and remove the class to hide it)
+})
