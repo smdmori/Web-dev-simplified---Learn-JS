b59b85bcfe2f4050cd2feda5fbf7ca315dad31ab
smdmori <mortezasamadi1371@gmail.com>
2023-03-09 19:36:59 +0330

    refactor

--- piano-midi/app.js
+++ piano-midi/app.js
@@ -1,26 +1,25 @@
 const audioContext = new AudioContext()
 
 const NOTE_DETAILS = [
-
   // Go to comment in playNotes() for more information
   // { note: 'C', key: 'Z', frequency: 261.626 },
-  
+
   // Cleaner Code
   { note: 'C', key: 'Z', frequency: 261.626, active: false },
   { note: 'Db', key: 'S', frequency: 277.183, active: false },
-  { note: 'D', key: 'X', frequency: 293.665 , active: false},
-  { note: 'Eb', key: 'D', frequency: 311.127 , active: false},
-  { note: 'E', key: 'C', frequency: 329.628 , active: false},
-  { note: 'F', key: 'V', frequency: 349.228 , active: false},
-  { note: 'Gb', key: 'G', frequency: 369.994 , active: false},
-  { note: 'G', key: 'B', frequency: 391.995 , active: false},
-  { note: 'Ab', key: 'H', frequency: 415.305 , active: false},
-  { note: 'A', key: 'N', frequency: 440 , active: false},
-  { note: 'Bb', key: 'J', frequency: 466.164 , active: false},
-  { note: 'B', key: 'M', frequency: 493.883 , active: false},
+  { note: 'D', key: 'X', frequency: 293.665, active: false },
+  { note: 'Eb', key: 'D', frequency: 311.127, active: false },
+  { note: 'E', key: 'C', frequency: 329.628, active: false },
+  { note: 'F', key: 'V', frequency: 349.228, active: false },
+  { note: 'Gb', key: 'G', frequency: 369.994, active: false },
+  { note: 'G', key: 'B', frequency: 391.995, active: false },
+  { note: 'Ab', key: 'H', frequency: 415.305, active: false },
+  { note: 'A', key: 'N', frequency: 440, active: false },
+  { note: 'Bb', key: 'J', frequency: 466.164, active: false },
+  { note: 'B', key: 'M', frequency: 493.883, active: false }
 ]
 
-document.addEventListener('keydown', e=> {
+document.addEventListener('keydown', e => {
   if (e.repeat) return
 
   const keyCode = e.code
@@ -37,7 +36,7 @@ document.addEventListener('keyup', e => {
   const noteDetail = getNoteDetails(keyCode)
 
   if (noteDetail == null) return
-  
+
   noteDetail.active = false
   playNotes()
 })
@@ -73,7 +72,7 @@ function startNote(noteDetail, gain) {
   gainNode.gain.value = gain
   const oscillator = audioContext.createOscillator()
   oscillator.frequency.value = noteDetail.frequency
-  oscillator.type = 'sine'
+  oscillator.type = 'square'
   oscillator.connect(gainNode).connect(audioContext.destination)
   oscillator.start()
   noteDetail.oscillator = oscillator
