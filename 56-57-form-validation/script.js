634552ec34c0a00ef9a52991124b8af8cc1a2058
smdmori <mortezasamadi1371@gmail.com>
2023-03-12 19:37:42 +0330

    started exercise

--- 56-57-form-validation/script.js
+++ 56-57-form-validation/script.js
@@ -1,14 +1,53 @@
 // TODO: Select all elements needed
 //    Use the HTML to figure out what classes/ids will work best for selecting each element
+const form = document.querySelector('#form')
+const username = document.querySelector('#username')
+const password = document.querySelector('#password')
+const passwordConfirm = document.querySelector('#password-confirmation')
+const terms = document.querySelector('#terms')
+
+console.log('form', form)
 
 // TODO: Create an event listener for when the form is submitted and do the following inside of it.
-//    TODO: Create an array to store all error messages and clear any old error messages
-//    TODO: Define the following validation checks with appropriate error messages
-//      1. Ensure the username is at least 6 characters long
-//      2. Ensure the password is at least 10 characters long
-//      3. Ensure the password and confirmation password match
-//      4. Ensure the terms checkbox is checked
-//    TODO: If there are any errors then prevent the form from submitting and show the error messages
+form.addEventListener('submit', e => {
+  e.preventDefault()
+
+  //    TODO: Create an array to store all error messages and clear any old error messages
+  const errorMessages = []
+
+  //    TODO: Define the following validation checks with appropriate error messages
+  //      1. Ensure the username is at least 6 characters long
+  const usernameValue = username.value
+  console.log('usernameValue', usernameValue)
+  if (usernameValue.length < 6) {
+    errorMessages.push('Username should be at least 6 characters.')
+  }
+
+  //      2. Ensure the password is at least 10 characters long
+  const passwordValue = password.value
+  console.log('passwordValue', passwordValue)
+  if (passwordValue.length < 10) {
+    errorMessages.push('Password should be at least 10 characters.')
+  }
+
+  //      3. Ensure the password and confirmation password match
+  const passwordConfirmValue = passwordConfirm.value
+  console.log('passwordConfirmValue', passwordConfirmValue)
+  if (passwordConfirmValue === passwordValue) {
+    errorMessages.push('Password should match.')
+  }
+
+  //      4. Ensure the terms checkbox is checked
+  if (terms.value !== true) {
+    errorMessages.push('Terms must be accepted.')
+  }
+
+  //    TODO: If there are any errors then prevent the form from submitting and show the error messages
+  if (errorMessages.length) {
+  }
+
+  console.log('e', e)
+})
 
 // TODO: Define this function
 function clearErrors() {
