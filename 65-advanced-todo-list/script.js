ffc47201556729d20be972de0e53508b5f12a8bf
smdmori <mortezasamadi1371@gmail.com>
2023-05-13 12:07:10 +0330

    finished advanced todo, following along

--- 65-advanced-todo-list/script.js
+++ 65-advanced-todo-list/script.js
@@ -5,19 +5,37 @@ const list = document.querySelector('#list')
 const template = document.querySelector('#list-item-template')
 const LOCAL_STORAGE_PREFIX = 'ADVANCED-TODO-LIST'
 const TODOS_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}-todos`
-const allTodos = loadTodos()
+let allTodos = loadTodos()
 allTodos.forEach(renderTodo)
 
+// delete todo
+list.addEventListener('click', e => {
+  if (!e.target.matches('[data-button-delete]')) return
+
+  const parent = e.target.closest('.list-item')
+  const todoId = parent.dataset.todoId
+
+  // remove todo from the list
+  allTodos = allTodos.filter(t => t.id !== todoId)
+
+  // remove todo from the screen
+  parent.remove()
+
+  // save todos
+  saveTodos()
+})
+
 list.addEventListener('change', e => {
-  if (!e.target.matches(['data-list-item-checkbox'])) return
+  if (!e.target.matches('[data-list-item-checkbox]')) return
 
   // grab the todo that is clicked on
   const parent = e.target.closest('.list-item')
   const todoId = parent.dataset.todoId
   const todo = allTodos.find(t => t.id === todoId)
 
-  todo.completed = e.target.checked
   // set the completed property equal to checkbox value
+  todo.completed = e.target.checked
+
   // save the updated value
   saveTodos()
 })
@@ -47,10 +65,15 @@ form.addEventListener('submit', e => {
 function renderTodo(todo) {
   const templateClone = template.content.cloneNode(true)
   const listItem = templateClone.querySelector('.list-item')
-  const textElement = templateClone.querySelector('[data-list-item-text]')
-
   listItem.dataset.todoId = todo.id
+
+  const textElement = templateClone.querySelector('[data-list-item-text]')
   textElement.innerText = todo.name
+
+  // complete todo
+  const checkbox = templateClone.querySelector('[data-list-item-checkbox]')
+  checkbox.checked = todo.completed
+
   list.appendChild(templateClone)
 }
 
@@ -64,6 +87,3 @@ function loadTodos() {
 function saveTodos() {
   localStorage.setItem(TODOS_STORAGE_KEY, JSON.stringify(allTodos))
 }
-
-// delete todo
-// complete todo
